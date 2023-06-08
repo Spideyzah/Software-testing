90f4dd7b18bb50c33f357217ab0043bec3d7dc1c
Zahidahmed <107020524+Spideyzah@users.noreply.github.com>
2023-06-08 23:43:09 +0530

    Create openswiggy.java

---
+++ openswiggy.java
@@ -0,0 +1,15 @@
+package selenium_prg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class openswiggy {
+	public static void main(String[] args) {
+		
+		System.out.println("hai");
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.swiggy.com/");
+		driver.manage().window().maximize();
+	}
+}
